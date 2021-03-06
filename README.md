cdc5f530d320bc5ca48f43a3d97f13a0e29b5b1b
Jacob Christian Poulsen <jp@dbc.dk>
2021-03-06 13:37:03 +0100

    Extended with different test Methods for simplified testing and not easy output of classpath. Tryed loading the graalJS with the system class loader.

--- README.md
+++ README.md
@@ -12,6 +12,17 @@ mvn package
 **Run**
 ```bash
 /path/to/graalvm/graalvm-ce-java11-21.0.0.2/bin/java -cp target/graalvm/graal-sdk-21.0.0.2.jar:/target/graalvm/icu4j-67.1.jar:target/graalvm/js-21.0.0.2.jar:target/graalvm/js-scriptengine-21.0.0.2.jar:target/graalvm/regex-21.0.0.2.jar:target/graalvm/truffle-api-21.0.0.2.jar -jar /path/to/payara-micro-5.2021.1.jar target/payara-graalvm-js-1.0-SNAPSHOT.war
+
+
+/home/ja7/opt/graalvm-ce-java11-21.0.0.2/bin/java -cp /home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/graal-sdk-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/icu4j-67.1.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/js-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/js-scriptengine-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/regex-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/truffle-api-21.0.0.2.jar -jar ./payara-micro-5.2021.1.jar /home/ja7/src/dbc/head/payara-graalvm-js/target/payara-graalvm-js-1.0-SNAPSHOT.war
+
+/home/ja7/opt/graalvm-ce-java11-21.0.0.2/bin/java -cp /home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/graal-sdk-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/icu4j-67.1.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/js-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/js-scriptengine-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/regex-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/truffle-api-21.0.0.2.jar -jar ./payara-micro-5.25.0.jar --addlibs /home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/graal-sdk-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/icu4j-67.1.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/js-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/js-scriptengine-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/regex-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/truffle-api-21.0.0.2.jar target/payara-graalvm-js-1.0-SNAPSHOT.war
+```
+                                                                              
+This seams to work
+```bash
+mvn clean && mvn package &&  /home/ja7/opt/graalvm-ce-java11-21.0.0.2/bin/java -cp /home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/graal-sdk-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/icu4j-67.1.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/js-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/js-scriptengine-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/regex-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/truffle-api-21.0.0.2.jar -jar ./payara-micro-5.25.0.jar --addlibs /home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/graal-sdk-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/icu4j-67.1.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/js-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/js-scriptengine-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/regex-21.0.0.2.jar:/home/ja7/src/dbc/head/payara-graalvm-js/target/graalvm/truffle-api-21.0.0.2.jar:/home/ja7/opt/graalvm/lib/graalvm/graaljs-launcher.jar:/home/ja7/opt/graalvm/languages/js/graaljs.jar:$G/lib/truffle/truffle-api.jar:$G/languages/js/graaljs.jar:$G/languages/js/trufflenode.jar target/payara-graalvm-js-1.0-SNAPSHOT.war
+
 ```
 **Verification**
 
--- src/main/java/dk/dbc/incubator/Resources.java
+++ src/main/java/dk/dbc/incubator/Resources.java
@@ -1,5 +1,6 @@
 package dk.dbc.incubator;
 
+import org.graalvm.home.HomeFinder;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Value;
 
@@ -11,10 +12,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.text.Collator;
+import java.util.Collection;
+import java.util.TreeSet;
 
 @Stateless
 @Path("resources")
 public class Resources {
+    final private String polyglotPrefix    = "\n/polygot     : ";
+    final private String polyglotSysPrefix = "\n/polygot-sys : ";
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -24,12 +32,103 @@ public class Resources {
     }
 
     @GET
+    @Path("sys")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getEngineSystemCL(@QueryParam("engine") String engineAlias) {
+
+        final ClassLoader oldClassLoader = Thread.currentThread().getContextClassLoader();
+        try {
+            Thread.currentThread().setContextClassLoader(ClassLoader.getSystemClassLoader());
+
+            final ScriptEngine engine = new ScriptEngineManager().getEngineByName(engineAlias);
+            return "engine for " + engineAlias + " is: " + engine + "\n";
+        } finally {
+            Thread.currentThread().setContextClassLoader(oldClassLoader);
+        }
+    }
+
+    @GET
     @Path("polyglot")
     @Produces(MediaType.TEXT_PLAIN)
     public String getPolyglot(@QueryParam("engine") String engineAlias) {
-        try (Context context = Context.newBuilder().allowExperimentalOptions(true).build()) {
-            final Value jsValue = context.eval("js", "'hello world from javascript'");
-            return jsValue.asString();
+        try {
+            try (Context context = Context.newBuilder().allowExperimentalOptions(true).build()) {
+                final Value jsValue = context.eval("js", "'hello world from javascript'");
+                return polyglotPrefix +jsValue.asString() + "\n";
+            }
+        } catch( Throwable e ) {
+            e.printStackTrace();
+            return polyglotPrefix+"Unable to Load Polyglot Exception : " + e.getMessage() + "\n";
         }
     }
+
+    @GET
+    @Path("polyglot-sys")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getPolyglotSystemCl(@QueryParam("engine") String engineAlias) {
+
+        try {
+            final ClassLoader oldClassLoader = Thread.currentThread().getContextClassLoader();
+            Thread.currentThread().setContextClassLoader(ClassLoader.getSystemClassLoader());
+
+            try (Context context = Context.newBuilder().allowExperimentalOptions(true).build()) {
+                final Value jsValue = context.eval("js", "'hello world from javascript'");
+                return polyglotSysPrefix + jsValue.asString() + "\n";
+            } finally {
+                Thread.currentThread().setContextClassLoader(oldClassLoader);
+            }
+
+        } catch( Throwable e ) {
+            return polyglotSysPrefix+"Unable to Load Polyglot Exception : " + e.getMessage() + "\n";
+        }
+    }
+
+    @GET
+    @Path("ja")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String dd () {
+        org.graalvm.home.HomeFinder hf= HomeFinder.getInstance();
+        return hf.getVersion() + hf.getToolHomes();
+    }
+
+    @GET
+    @Path("this")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String dumpThisCl( ) {
+        return dumpClassLoader( this.getClass().getClassLoader() );
+    }
+
+    @GET
+    @Path("this-sys")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String dumpSysCl () {
+        return dumpClassLoader( ClassLoader.getSystemClassLoader());
+    }
+
+    public String dumpClassLoader( ClassLoader cl ) {
+        StringBuilder sb = new StringBuilder();
+        sb.append('\n');
+        sb.append(cl.toString()).append('\n');
+
+        try {
+            URL[] urls = ((URLClassLoader) cl).getURLs();
+            Collection<String> sort_urls = new TreeSet<String>(Collator.getInstance());
+            for (URL url : urls) {
+                sort_urls.add(url.getFile());
+            }
+
+
+            sb.append("size : ").append(sort_urls.size()).append('\n');
+            for (String s : sort_urls) {
+                sb.append(s).append('\n');
+            }
+        } catch( Exception e) {
+            sb.append("Only able to list URLClassLoader path\n");
+        }
+        return sb.toString();
+    }
+
+
+
+
 }
